975bd28f1aa25e063014b240848390c2fc9cb2b0
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-09-04 01:40:54 -0400

    ajuste de identacion de prisma

--- Backend/src/modules/Alumnos/infrastructure/prisma/CreateALumnoPrisma.ts
+++ Backend/src/modules/Alumnos/infrastructure/prisma/CreateALumnoPrisma.ts
@@ -6,7 +6,6 @@ import { Carrera } from '@/core/value-objects/Carrera.value'
 export class CreateAlumnoPrisma implements AlumnoRepository {
   async create(data: Alumno): Promise<Alumno> {
     return await prisma.$transaction(async (prisma) => {
-
       const user = await prisma.user.findFirst({
         where: {
           id_user: data.getIdUser,
@@ -17,83 +16,83 @@ export class CreateAlumnoPrisma implements AlumnoRepository {
           }
         }
       })
-      
+
       if (!user) throw new Error('User not found')
-        
-        const alumno = await prisma.alumno.create({
-          data: {
-            id_user: data.getIdUser,
-            carrera: data.getCarrera.getCarrera,
-          },
-          
-          select: {
-            id_alumno: true,
-            id_user: true,
-            carrera: true
-          }
-        })
-        
-        await prisma.user.update({
-          where: {
-            id_user: data.getIdUser
-          },
-          
-          data: {
-            is_active: true
-          }
-        })
-        
-        return new Alumno(
-          alumno.id_alumno,
-          alumno.id_user,
-          new Carrera(alumno.carrera as string)
-        )
+
+      const alumno = await prisma.alumno.create({
+        data: {
+          id_user: data.getIdUser,
+          carrera: data.getCarrera.getCarrera
+        },
+
+        select: {
+          id_alumno: true,
+          id_user: true,
+          carrera: true
+        }
       })
+
+      await prisma.user.update({
+        where: {
+          id_user: data.getIdUser
+        },
+
+        data: {
+          is_active: true
+        }
+      })
+
+      return new Alumno(
+        alumno.id_alumno,
+        alumno.id_user,
+        new Carrera(alumno.carrera as string)
+      )
+    })
+  }
+  async findById(id_user: string): Promise<Alumno | null> {
+    const alumno = await prisma.alumno.findUnique({
+      where: {
+        id_user: id_user
+      },
+
+      select: {
+        id_alumno: true,
+        id_user: true,
+        carrera: true
       }
-      async findById(id_user: string): Promise<Alumno | null> {
-        const alumno = await prisma.alumno.findUnique({
-          where: {
-            id_user: id_user
-          },
-          
-          select: {
-            id_alumno: true,
-            id_user: true,
-            carrera: true
-          }
-        })
-        
-        if (!alumno) return null
-        return new Alumno(
-          alumno.id_alumno,
-          alumno.id_user,
-          new Carrera(alumno.carrera as string)
-        )
-      }
-      
-      async findAll(): Promise<Alumno[]> {
-        const alumnos = await prisma.alumno.findMany({
-          include: {
-            user: true
-          },
-          where: {
-            user: {
-              roles: {
-                some: {
-                  rol: 'ALUMNO'
-                }
-              }
+    })
+
+    if (!alumno) return null
+    return new Alumno(
+      alumno.id_alumno,
+      alumno.id_user,
+      new Carrera(alumno.carrera as string)
+    )
+  }
+
+  async findAll(): Promise<Alumno[]> {
+    const alumnos = await prisma.alumno.findMany({
+      include: {
+        user: true
+      },
+      where: {
+        user: {
+          roles: {
+            some: {
+              rol: 'ALUMNO'
             }
           }
-        })
-        
-        return alumnos.map((alumno) => {
-          return new Alumno(
-            alumno.id_alumno,
-            alumno.id_user,
-            new Carrera(alumno.carrera as string)
-          )
-        })
+        }
+      }
+    })
+
+    return alumnos.map((alumno) => {
+      return new Alumno(
+        alumno.id_alumno,
+        alumno.id_user,
+        new Carrera(alumno.carrera as string)
+      )
+    })
   }
 
   async deleteAlumno(id_user: string): Promise<Alumno | null> {
@@ -152,5 +151,4 @@ export class CreateAlumnoPrisma implements AlumnoRepository {
       )
     })
   }
-
 }
