f12a9d76d865d9b49f789a857b874eed31052d6b
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-09-08 14:17:43 -0400

    manejo de error

--- Backend/src/core/value-objects/Nombre.value.ts
+++ Backend/src/core/value-objects/Nombre.value.ts
@@ -1,9 +1,12 @@
+import { ValueError } from "../errors/value.error"
+
 export class Nombre {
   private readonly nombre: string
 
   constructor(nombre: string) {
-    if (!nombre) throw new Error('Nombre is required')
-    if (typeof nombre !== 'string') throw new Error('Nombre must be a string')
+    if (!nombre) throw new ValueError('Nombre is required')
+    if (typeof nombre !== 'string') throw new ValueError('Nombre must be a string')
+    if (nombre.length < 2) throw new ValueError('Nombre must be at least 2 characters')
     
     this.nombre = nombre
   }
